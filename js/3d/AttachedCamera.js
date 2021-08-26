e667be9e598035688d60a53f605f3e83fc29890f
Michał Ziober <ziobermichal@gmail.com>
2021-08-26 22:13:01 +0200

    Improved AttachedCamera.js class

--- js/3d/AttachedCamera.js
+++ js/3d/AttachedCamera.js
@@ -1,14 +1,18 @@
+/**
+ * Base class to handle 3D camera attached to HTML canvas.
+*/
 class AttachedCamera {
     constructor(scene, canvas) {
         this.canvas = canvas;
         this.scene = scene;
+        this.position = new BABYLON.Vector3(20, 200, 400);
         this.camera = this.createNewCamera();
+        this.attach();
     }
 
     createNewCamera() {
         var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), this.scene);
-        camera.setPosition(new BABYLON.Vector3(20, 200, 400));
-        camera.attachControl(this.canvas, true);
+        camera.setPosition(this.position);
         camera.upperBetaLimit = (Math.PI / 2) * 0.99;
         
         return camera;
